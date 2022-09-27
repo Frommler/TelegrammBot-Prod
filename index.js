73fcb9df7ec512fc541999d67d09945d4f4692f9
Alex Vakula <albanec777@meta.ua>
2022-09-27 19:21:22 +0200

    search product ignore case

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ Bot.start(ctx => {
   ctx.replyWithSticker("https://www.gstatic.com/webp/gallery/1.webp");
 });
 
-Bot.hears(/[A-Z]+/, ctx => {
+Bot.hears(/[A-Z]+/i, ctx => {
+  console.log("HERE");
   fetch("http://localhost:3000/get-product-by/" + ctx.message.text)
   .then(res => res.json())
   .then(data => {
+    if(data.length == 0){
+      ctx.reply("Sorry,we don`t have these products");
+      return;
+    }
     data.forEach(element => {
       ctx.reply(element.name + " " + element.price + "\n" + element.image);
     });
